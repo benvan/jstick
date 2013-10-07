f93b47c59eefe1ad1de11cc9d2db8e7b6cab4477
Ben van Enckevort <github@benvan.co.uk>
2013-10-07 20:44:44 +0100

    added externally accessible activate/drag/release methods for daisy-chaining

--- jstick.js
+++ jstick.js
@@ -1,3 +1,7 @@
+var extend = function(a,b){ for (var i in b) a[i] = b[i]; return a; };
+var shallowClone = function(o){ return extend({}, o); };
+var noop = function(){};
+
 var Point = function(x,y){
     this.x = x;
     this.y = y;
@@ -6,42 +10,35 @@ var Point = function(x,y){
     };
 };
 
-var JStick = function(opts){
 
-    var extend = function(a,b){ for (var i in b) a[i] = b[i]; return a; };
-    var shallowClone = function(o){ return extend({}, o); };
+var JStick = function(opts){
 
     var bind = function(joystick){
         joystick.settings.target.addEventListener('mousedown',function(ev){
             if (joystick.enabled && ev.button == 0){
-                joystick.settings.onactivate.apply(joystick);
-                joystick.active = true;
-                joystick.start = new Point(ev.pageX, ev.pageY);
+                joystick.activateAt({x: ev.pageX, y: ev.pageY});
             }
         });
-        document.addEventListener('mouseup', function(ev){
+        document.addEventListener('mouseup', function(){
             if (joystick.enabled){
-                var wasActive = joystick.active;
-                joystick.active = false;
-                if (wasActive) joystick.settings.onrelease.apply(joystick);
+                joystick.release();
             }
         });
         document.addEventListener('mousemove', function(ev){
             if (joystick.enabled){
-                joystick.now.x = ev.pageX;
-                joystick.now.y = ev.pageY;
-                if (joystick.active) joystick.settings.ondrag.apply(joystick);
+                joystick.dragTo({x: ev.pageX, y: ev.pageY});
             }
         });
     };
 
     var diff = function(dimension){
         return function(sensitivity){
-            return (this.now[dimension] - this.start[dimension]) / (sensitivity ? Math.pow(10,sensitivity) : 1) * (( this.settings.invertY && dimension == 'y') ? -1 : 1);
+            var inversion = (( this.settings.invertY && dimension == 'y') ? -1 : 1);
+            return (this.now[dimension] - this.start[dimension])
+                   / (sensitivity ? Math.pow(10,sensitivity) : 1) * inversion;
         }
     };
 
-    var noop = function(){};
     var defaults = {
         target: document,
         invertY: true,
@@ -49,6 +46,7 @@ var JStick = function(opts){
         onactivate: noop,
         ondrag: noop
     };
+
     var settings = extend(defaults, opts);
 
     var joystick = extend(this, {
@@ -57,6 +55,25 @@ var JStick = function(opts){
         active: false,
         start: new Point(0,0),
         now: new Point(0,0),
+        activateAt: function(position){
+            this.start = new Point(position.x, position.y);
+            this.active = true;
+            this.settings.onactivate.apply(joystick);
+        },
+        release: function(){
+            var wasActive = this.active;
+            this.active = false;
+            if (wasActive) this.settings.onrelease.apply(joystick);
+        },
+        dragTo: function(position){
+            this.now.x = position.x;
+            this.now.y = position.y;
+            if (this.active) this.settings.ondrag.apply(joystick);
+        },
+        moveTo: function(offset){
+            this.now.x = this.start.x + offset.x;
+            this.now.y = this.start.y + (offset.y * (this.settings.invertY ? -1 : 1));
+        },
         dx: diff('x'),
         dy: diff('y')
     });
